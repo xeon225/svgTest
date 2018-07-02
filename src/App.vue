465fc32eddd65cb590a656d98a43ce5813599286
王磊 <11169514@qq.com>
2018-07-02 10:08:53 +0800

    test 01

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="flex-container vfull">
+    <div class="appLeft">
+      1
+    </div>
+    <div class="appRight flex1"></div>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  height:100%;
+}
+.appLeft{
+  width:130px;
+  background: #7762a4;
+}
+</style>
